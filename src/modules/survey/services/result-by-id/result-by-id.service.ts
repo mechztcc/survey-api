dea859296fd54ee9e762bbf58d338df048046d6a
mechztcc <dojathan@gmail.com>
2023-12-27 16:40:14 -0500

    feat: result by id

--- src/modules/survey/services/result-by-id/result-by-id.service.ts
+++ src/modules/survey/services/result-by-id/result-by-id.service.ts
@@ -12,15 +12,33 @@ export class ResultByIdService {
 
   async execute(id: number) {
     const surveyVotes = await this.votesRepository
-    .createQueryBuilder('userVote')
-    .select('survey.id', 'surveyId')
-    .addSelect('COUNT(CASE WHEN userVote.vote = \'yes\' THEN 1 ELSE NULL END)', 'yesCount')
-    .addSelect('COUNT(CASE WHEN userVote.vote = \'no\' THEN 1 ELSE NULL END)', 'noCount')
-    .leftJoin('userVote.survey', 'survey')
-    .where('survey.status = :status', { status: 'opened' })
-    .andWhere('survey.id = :surveyId', { surveyId: id })
-    .groupBy('survey.id')
-    .getRawOne();
+      .createQueryBuilder('userVote')
+      .select('survey.id', 'surveyId')
+      .addSelect(
+        "COUNT(CASE WHEN userVote.vote = 'yes' THEN 1 ELSE NULL END)",
+        'yesCount',
+      )
+      .addSelect(
+        "COUNT(CASE WHEN userVote.vote = 'no' THEN 1 ELSE NULL END)",
+        'noCount',
+      )
+      .addSelect(
+        "SUM(CASE WHEN userVote.vote IN ('yes', 'no') THEN 1 ELSE 0 END)",
+        'totalCount',
+      )
+      .addSelect(
+        "CAST((COUNT(CASE WHEN userVote.vote = 'yes' THEN 1 ELSE NULL END) * 100.0) / SUM(CASE WHEN userVote.vote IN ('yes', 'no') THEN 1 ELSE 0 END) AS DECIMAL(10,2))",
+        'yesPercentage',
+      )
+      .addSelect(
+        "CAST((COUNT(CASE WHEN userVote.vote = 'no' THEN 1 ELSE NULL END) * 100.0) / SUM(CASE WHEN userVote.vote IN ('yes', 'no') THEN 1 ELSE 0 END) AS DECIMAL(10,2))",
+        'noPercentage',
+      )
+      .leftJoin('userVote.survey', 'survey')
+      .where('survey.status = :status', { status: 'opened' })
+      .andWhere('survey.id = :surveyId', { surveyId: id }) // Filtra pelo ID da Survey
+      .groupBy('survey.id')
+      .getRawOne();
 
     return surveyVotes;
   }

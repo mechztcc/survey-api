8a3b344ff9373165d31810083fc7ae11f98e4b99
mechztcc <dojathan@gmail.com>
2023-12-28 12:47:10 -0500

    fix: changes on cron

--- src/core/seeds/db.seeder.ts
+++ src/core/seeds/db.seeder.ts
@@ -1,30 +1,59 @@
 import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Survey } from 'src/modules/survey/entities/survey.entity';
+import {
+  UserVote,
+  VoteAnswer,
+} from 'src/modules/users/entities/user-vote.entity';
 import { User } from 'src/modules/users/entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, getConnection } from 'typeorm';
 
 @Injectable()
 export class DbSeeder {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+
+    @InjectRepository(Survey)
+    private surveysRepository: Repository<Survey>,
+
+    @InjectRepository(UserVote)
+    private votesRepository: Repository<UserVote>,
   ) {}
   async execute() {
-    const dummy: User[] = [];
-    for (let index = 0; index < 10; index++) {
-      dummy.push({
-        document: '09174489445',
-        email: faker.internet.email(),
-        name: faker.person.fullName(),
-        password: faker.internet.password(),
-        profile: 'guest',
-        surveys: [],
+    const surveysToInsert = [];
+
+    const user: User = {
+      document: '09174489445',
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password(),
+      profile: 'guest',
+      surveys: [],
+      userVote: [],
+    };
+
+    this.usersRepository.create(user);
+    await this.usersRepository.save(user);
+
+    for (let i = 0; i < 10; i++) {
+      const survey: Survey = {
+        question: faker.lorem.sentence(),
+        expires_at: faker.date.future(),
+        votes: 0,
+        user,
         userVote: [],
-      });
+        status: 'opened',
+      };
+      surveysToInsert.push(survey);
     }
 
-    const users = this.usersRepository.insert([...dummy]);
-    return users;
+    await this.surveysRepository
+      .createQueryBuilder()
+      .insert()
+      .into('survey') // Nome da tabela 'survey'
+      .values(surveysToInsert)
+      .execute();
   }
 }
--- src/core/seeds/seeds.module.ts
+++ src/core/seeds/seeds.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DbSeeder } from './db.seeder';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/modules/users/entities/user.entity';
+import { Survey } from 'src/modules/survey/entities/survey.entity';
+import { UserVote } from 'src/modules/users/entities/user-vote.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Survey,UserVote])],
   providers: [DbSeeder],
   exports: [DbSeeder],
 })
